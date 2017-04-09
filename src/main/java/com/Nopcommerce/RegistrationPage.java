aa26133cba0d5ae93e96a7771de2bd6cd0d910a9
keshashivam <shivamkesha@gmail.com>
2017-04-09 13:28:14 +0100

    test update

---
+++ src/main/java/com/Nopcommerce/RegistrationPage.java
@@ -0,0 +1,13 @@
+package com.Nopcommerce;
+
+/**
+ * Created by user on 4/9/2017.
+ */
+public class RegistrationPage
+{
+    public void userShouldBeRegiusterSuccessfully()
+    {
+        
+
+    }
+}
